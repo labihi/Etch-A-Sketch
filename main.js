97ecfb6db270b45c5bbfed2dfcda5cf547f8d77e
labihi <mekhipipher@gmail.com>
2022-02-13 23:33:21 +0100

    Created the standard layout and added some styling, the grid is also properly created

---
+++ main.js
@@ -0,0 +1,17 @@
+const gridContainer = document.getElementsByClassName("grid-container")[0]; //Where the grid should be created inside
+
+function createGrid(number){
+    for(let i = 0; i < (number); i++){
+        const gridColumn = document.createElement("div");
+        gridColumn.classList.add("grid-column");
+        gridContainer.appendChild(gridColumn);
+        for(let j = 0; j < number; j++){
+            const gridElement = document.createElement("div");
+            gridElement.classList.add("grid-element");
+            gridColumn.appendChild(gridElement);
+        }
+    }
+}
+
+createGrid(8)
+

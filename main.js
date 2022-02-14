1da8f2ccb562cc4de52a7d2acb2c28e42eae9d76
labihi <mekhipipher@gmail.com>
2022-02-14 13:14:09 +0100

    Added clear grid, background color filler and change of pen color

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const gridContainer = document.getElementsByClassName("grid-container")[0]; //Where the grid should be created inside
-let gridSize = 8;
+const clearButton = document.getElementById("clear-button");
+const fillButton = document.getElementById("fill-button");
+const eraserButton = document.getElementById("eraser-button");
+const slider = document.getElementById("myRange");
+const penColorPicker = document.getElementById("pen-color");
+const backgroundColorPicker = document.getElementById("background-color");
+let output = document.getElementById("demo");
+
+const DEFAULT_GRID_SIZE = 8;
 
 function createGrid(number) {
     for (let i = 0; i < number; i++) {
@@ -20,16 +28,48 @@ function createGrid(number) {
 
 function changeColor(evt) {
     if (evt.type === "mousedown") {
-        this.style.backgroundColor = "yellow";
+        this.style.backgroundColor = penColorPicker.value;
     }
 }
 
-let slider = document.getElementById("myRange");
-let output = document.getElementById("demo");
+
 output.innerHTML = `${slider.value} x ${slider.max}`;
 
 slider.oninput = function () {
     output.innerHTML = `${this.value} x ${slider.max}`;
+    updateGrid();
 };
 
-createGrid(gridSize);
+function clearGrid(){
+    gridContainer.innerHTML = "";
+}
+
+function resetGrid(){
+    clearGrid();
+    createGrid(slider.value);
+    gridContainer.style.backgroundColor = "transparent";
+}
+
+function updateGrid(){
+    clearGrid();
+    createGrid(slider.value);
+}
+
+clearButton.addEventListener("click", () => {
+    resetGrid();
+});
+
+fillButton.addEventListener("click", () => {
+    gridContainer.style.backgroundColor = backgroundColorPicker.value;
+});
+
+eraserButton.addEventListener("click", () => {
+    const gridElement = document.getElementsByClassName("grid-element");
+    for(let i = 0; i < gridElement.length; i++){
+        gridElement[i].addEventListener("click", () => {
+            gridElement[i].style.backgroundColor = "transparent";
+        });
+    }
+});
+
+createGrid(DEFAULT_GRID_SIZE);
